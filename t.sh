23baffb0cc9dbc673c0d27f8652b37690679f542
Serarinne <160508714+Serarinne@users.noreply.github.com>
2024-09-20 03:53:25 +0700

    Create t.sh

---
+++ t.sh
@@ -0,0 +1,5 @@
+curl --location 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=AIzaSyAIvieoiQa7F58HXHZJQRWlnp4AY1kfz9U' \
+--header 'Content-Type: text/plain' \
+--data '{"contents": [{"parts":[{"text": 
+        "Buatkan artikel singkat mengenai cara berkendara dengan aman"
+        }]}],"safetySettings": [{"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT","threshold": "BLOCK_NONE"},{"category": "HARM_CATEGORY_HATE_SPEECH","threshold": "BLOCK_NONE"},{"category": "HARM_CATEGORY_HARASSMENT","threshold": "BLOCK_NONE"},{"category": "HARM_CATEGORY_DANGEROUS_CONTENT","threshold": "BLOCK_NONE"}]}'
